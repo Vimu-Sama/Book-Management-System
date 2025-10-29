8cdf0479fde1320a03ecc4f7aecbecda4433fbd9
Vimu-Sama <42619785+Vimu-Sama@users.noreply.github.com>
2025-10-29 18:11:25 +0000

    Added model for Book

--- packages/models/book.go
+++ packages/models/book.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"github.com/Vimu-Sama/Book-Management-System/packages/config"
+	"github.com/jinzhu/gorm"
+)
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func Migrate() {
+	db := config.GetDB()
+	db.AutoMigrate(&Book{})
+}
