2a3384a8d51dd6f8616556021da934bc3132cf42
Vimu-Sama <42619785+Vimu-Sama@users.noreply.github.com>
2025-10-30 17:24:49 +0000

    Edited main file to listen and serve basic web server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/Vimu-Sama/Book-Management-System/packages/routes"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Printf("Hello World!")
+	if err:= godotenv.Load(); err!=nil{
+		panic(log) ;
+	}
+	appRouter := mux.NewRouter() ;
+	routes.BookManagementRoutes(appRouter);
+	http.Handle("/", appRouter);
+	if err:= http.ListenAndServe("LocalHost:"+ os.Getenv("PORT"), appRouter)
 }
